9219e846a95079b0bdd9236b5e3a520b5727ab71
ehas <ehasahamed@gmail.com>
2024-04-08 14:10:05 +0530

    index update

--- new/js/custom.js
+++ new/js/custom.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-    $(".down-menu").hover(function () { 
+    $(".down-menu").click(function () { 
    
       
-      $(".dropdown").slideToggle("fast"); 
+      $(".dropdown").slideToggle();
     });
+    $(".down-menu1").click(function () { 
+   
+      
+      $(".dropdown1").slideToggle();
+    });
+ 
+   
   });
+  $(document).ready(function() {
+    $(".fa-bars").click(function(e) {
+        $(".mobile-nav").slideToggle(); // Show/hide the mobile-nav menu
+    });
+});

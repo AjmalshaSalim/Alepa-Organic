67aba56c3155bc5c29bd1f3a8aa0ae4bbf3f8397
ehas <ehasahamed@gmail.com>
2024-04-08 17:56:09 +0530

    updation

--- new/js/custom.js
+++ new/js/custom.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
-    $(".down-menu").click(function () { 
+    $(".down-menu").hover(function () { 
    
       
-      $(".dropdown").slideToggle();
+      $(".dropdown").show();
     });
     $(".down-menu1").click(function () { 
    
       
       $(".dropdown1").slideToggle();
     });
- 
+    $("#stainlesss").hover(function () { 
    
+      
+      $("#desc1").slideToggle();
+    });
+    $("#Wildforest-img").hover(function () { 
+   
+      
+      $("#Wildforest").slideToggle();
+    });
+    $("#Multifloral-img").hover(function () { 
+   
+      
+      $("#Multifloral").slideToggle();
+    });
   });
   $(document).ready(function() {
     $(".fa-bars").click(function(e) {
